68e8d2770d44953116de0500d6faa5b93173c259
HarrisonKawagoe3960X <hyzitbios@hotmail.com>
2020-12-10 04:46:00 +0900

    make it better

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-import chromedriver_binary
 import json
 from flask import jsonify
 from Cryptodome import Random
